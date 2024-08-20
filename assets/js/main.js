216be22aca70c182682a2128c0293ab1b335782f
Ezequiel <tezequiel@gmail.com>
2024-08-19 23:51:17 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+(function() {
+  "use strict";
+
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+})();
